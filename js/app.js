8db3359f88c9d1db5fea9a7c0453e468980414e7
Clirim Ajeti <c.ajeti@intes.co>
2019-01-20 12:47:31 +0100

    Tribute Project

---
+++ js/app.js
@@ -0,0 +1,23 @@
+var menu = document.querySelectorAll(".menu-item");
+
+var showMenu = false;
+
+function show() {
+  showMenu = !showMenu;
+  if(showMenu){
+    for (var i = 0; i < menu.length; i++) {
+      menu[i].style.transition = "all 1s linear";
+      menu[i].style.margin = "1px";
+      menu[i].style.transform = "translateX(0)";
+      menu[i].style.opacity = "1";
+    }
+  } else {
+    for (var i = 0; i < menu.length; i++) {
+      menu[i].style.transition = "all 1s linear";
+      menu[i].style.margin = "1px";
+      menu[i].style.transform = "translateX(-500%)";
+      menu[i].style.opacity = "0";
+    }
+  }
+
+}
